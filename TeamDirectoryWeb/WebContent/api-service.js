273ac72a0a5d2338909cbd9410930627fae0dcce
syovchev <stephanyovchev@gmail.com>
2018-06-18 15:32:13 +0200

    Add Api Service. Atm it communicates with the mock api.

---
+++ TeamDirectoryWeb/WebContent/api-service.js
@@ -0,0 +1,45 @@
+const ApiService = (() => {
+    const api = 'https://private-d2efc-teamdirectory.apiary-mock.com/api'
+    
+    function $request (type, url, data) {
+        return $.ajax({
+            type,
+            dataType: 'json',
+            url,
+            data
+        });
+    }
+
+    return {
+        User: {
+            getById(id) {
+                let url = `${api}/users/${id}`;
+
+                return $request('GET', url, null);   
+            }
+        },
+        Comment: {
+            postComment(comment) {
+                let url = `${api}/comments/`;
+
+                return $request('POST', url, comment);
+            },
+            getComment() {
+                let url = `${api}/comments`;
+
+                return {
+                    byReceiverId(receiverId) {
+                        url += `?receiverId=${receiverId}`;
+
+                        return $request('GET', url, null);
+                    },
+                    byPosterId(posterId) {
+                        url += `?posterId=${posterId}`;
+
+                        return $request('GET', url, null);
+                    }
+                }
+            }
+        }
+    };
+})();
